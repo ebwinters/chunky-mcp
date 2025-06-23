82eed997eae4d1a8bc0550f2fd2c7a5b395b9896
Ethan Winters <ebwinters@comcast.net>
2025-06-23 07:41:35 -0700

    del tool

--- chunky_mcp/main.py
+++ chunky_mcp/main.py
@@ -1,8 +1,7 @@
 import types
 from mcp.server.fastmcp import FastMCP
 from mcp import types
-from chunky_mcp_utils import chunker, handle_large_response
-import requests
+from chunky_mcp_utils import chunker
 
 mcp = FastMCP("AsyncDemoServer")
 _chunker = chunker.ResponseChunker()
@@ -39,21 +38,6 @@ async def read_response_chunk(file_path: str, chunk_number: int = 0) -> list[typ
             text=f"Error reading chunk: {str(e)}"
         )]
 
-@mcp.tool()
-def get_employees() -> list[types.TextContent]:
-    """
-    Gets a list of all the employees in the system from the database
-    When the user asks for a list of employees, this tool is called.
-    """
-    # Simulate a large response
-    response = requests.get("https://microsoftedge.github.io/Demos/json-dummy-data/128KB.json")
-    response.raise_for_status()
-    response_data = response.json()
-    
-    return handle_large_response(
-        response_data,
-        get_employees.__name__
-    )
 def main():
     mcp.run(transport="stdio")
 
